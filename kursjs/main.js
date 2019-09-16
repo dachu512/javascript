61b48d597db718157c28531bfcf98d02ffe73888
wojtekzalewski <wbzwojciechzalewski@gmail.com>
2019-09-16 16:30:53 +0200

    część druga

--- kursjs/main.js
+++ kursjs/main.js
@@ -273,3 +273,207 @@ files.sort(function(a4,b4) {
 })
 
 console.log(files);
+
+// ***
+
+const product1 = {
+    name: "Kubek",
+    price: 50,
+    weight: 1 
+}
+
+const product2 = {
+    name: "plakat",
+    price: 30,
+    weight: 0.03
+}
+
+console.log(product1);
+console.log(product2);
+console.log("Produkt numer jeden to: " + product1.name);
+console.log("Produkt numer dwa to: " + product2.name);
+console.log("Produkty kosztują razem: " + (product1.price + product2.price));
+console.log("Produkty waźą razem: " + (product1.weight + product2.weight));
+
+// ***
+const currentUser = {
+    name: "Piotr",
+    surname: "Szwed",
+    email: "pioszwed@mail.pl",
+    www: "www.pszwed.pl",
+    userType: "developer",
+    show: function() {
+        console.log("Imię: " + this.name);
+        console.log("Nazwisko: " + this.surname);
+        console.log("Email: " + this.email);
+        console.log("Strona www: " + this.www);
+        console.log("Typ: " + this.userType);
+    }
+}
+
+currentUser.show();
+
+// ***
+
+const book = {
+    title: "Erynie",
+    author: "Marek Krajewski",
+    pageCount: 300,
+    publisher: "Znak",
+    showDetails: function() {
+        for (const i in this) {
+            if (typeof this[i] !== "function"){
+                console.log("Klucz: " + i);
+                console.log("Wartość: " + this[i]);
+            }
+        }
+    }
+}
+
+// ***
+
+const users = [
+    //id name surname email age value
+    [  1, "Shauna", "Bradnocke", "sbradnocke0@altervista.org", 20, 108.28 ],
+    [  2, "Mela", "Redman", "mredman1@nps.gov", 24, 267.37 ],
+    [  3, "Othelia", "Lemon", "olemon2@slashdot.org", 15, 748.06 ],
+    [  4, "Meier", "Cockell", "mcockell3@icio.us", 32, 1951.64 ],
+    [  5, "Shellysheldon", "Gronowe", "sgronowe4@cnbc.com", 16, 1040.54 ],
+    [  6, "Francisca", "Tofanini", "ftofanini5@gnu.org", 21, 1544.08 ],
+    [  7, "Cliff", "Underwood", "cunderwood6@addtoany.com", 10, 451.21 ],
+    [  8, "Caron", "Falshaw", "cfalshaw7@hugedomains.com", 27, 1968.72 ],
+    [  9, "Anitra", "Warters", "awarters8@intel.com", 12, 380.68 ],
+    [ 10, "Caitrin", "Baudrey", "cbaudrey9@ihg.com", 13, 1385.44 ]
+]
+
+function fixData(tab) {
+    const newTab = [];
+    for (const el of tab){
+        const ob ={
+            id: el[0],
+            name: el[1],
+            surname: el[2],
+            email: el[3],
+            age: el[4],
+            money: el[5]
+        }
+        newTab.push(ob);
+    }
+    return newTab;
+}
+
+const fixTab = fixData(users);
+console.log(fixTab);
+
+let money = 0;
+let age2 = 0;
+
+for(const el of fixTab){
+    money += el.money;
+    age2 += el.age2;
+}
+
+const summary = {
+    money: money,
+    age2: age2,
+    averageAge: age2/fixTab.length
+}
+
+// ***
+
+function Car(name, brand, engine, mile, age) {
+    this.name = name;
+    this.brand = brand;
+    this.engine = engine;
+    this.mile = mile;
+    this.age = age;
+}
+
+Car.prototype.printDetails = function() {
+    console.log("Nazwa: ", this.name);
+    console.log("Marka: " + this.brand);
+    console.log("Silnik: " + this.engine);
+    console.log("Przebieg: " + this.mile);
+    console.log("Wiek: " + this.age);
+}
+
+const car1 = new Car("Corolla", "Toyota", 1.4, 240000, 15);
+const car2 = new Car("Corsa", "Opel", 1.2, 120000, 11);
+const car3 = new Car("Grand Vitara", "Suzuki", 2.0, 198000, 12);
+
+// ***
+
+const Enemy = function(name, live, speed, attack,posX) {
+    this.name = name;
+    this.live = 5;
+    this.speed = speed;
+    this.attack = attack;
+    this.posX = posX;
+}
+
+Enemy.prototype.printName = function() {
+    return "Jestem " + this.name + ". " 
+}
+
+Enemy.prototype.move = function() {
+    this.posX = this.posX - this.speed;
+    console.log(this.printName() + "Znajduje się na pozycji" + this.posX);
+}
+
+Enemy.prototype.attackEnemy = function() {
+    console.log(this.printName() + "Atakuję gracza z pozycji " + this.posX + " z siłą " + this.attack);
+}
+Enemy.prototype.hit = function() {
+    this.live--
+    console.log(this.printName() + "Mam teraz życia " + this.live);
+}
+
+const e1 = new Enemy("Enemy1", 4, 10, 250);
+e1.move();
+e1.move();
+e1.move();
+e1.attackEnemy();
+
+const e2 = new Enemy("Enemy2", 3, 15, 250);
+e2.move();
+e2.move();
+e2.move();
+e2.attackEnemy();
+e2.hit();
+e2.hit();
+e2.hit();
+
+// ***
+
+String.prototype.sortText = function(char){
+    const tab = this.split(char);
+    tab.sort();
+
+    const newStr = tab.join(char);
+    return newStr;
+}
+
+console.log("Marcin|Ania|Piotrek|Beata".sortText('|'));
+
+// ***
+
+const ob = {
+    time: 2000,
+    pets: ["pies", "kot", "świnka"],
+    print: function() {
+        setTimeout(function() {
+            for(const pet of this.pets) {
+                console.log(pet.toUpperCase());
+            }
+        }.bind(this), this.time);
+    }
+}
+ob.print();
+
+// ***
+
+const h2 = document.querySelector('.tasks-for-you-cnt h2');
+
+console.log(h2);
+console.dir(h2);
+
