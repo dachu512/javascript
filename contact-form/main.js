c2abe168f7df5644d28d245754742d7bfefd98b2
Wojtek Zalewski <wbzwojciechzalewski@gmail.com>
2019-10-21 20:06:33 +0200

    zmiany tresci w labelach name i email

--- contact-form/main.js
+++ contact-form/main.js
@@ -1,1 +1,13 @@
-              
+const form = document.querySelector('#contactForm');      
+const inputs = form.querySelectorAll('input[required], textarea[required], select[required]');
+
+form.setAttribute('novalidate', true);
+[...inputs].forEach(elem =? {
+    elem.addEventListener('input', function() {
+        
+    })
+})
+
+var vi = "si senior";
+console.log("TCL: si senior", si senior)
+console.log("TCL: si", si)
